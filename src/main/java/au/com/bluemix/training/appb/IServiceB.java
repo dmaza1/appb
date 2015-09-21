0f0f9b7ee8242fd73a33425e4ddc6eb82efdc520
bluemixtraining1 <bluemixtraining1@gmail.com>
2015-09-21 14:46:33 +0800

    Add webservices

---
+++ src/main/java/au/com/bluemix/training/appb/IServiceB.java
@@ -0,0 +1,16 @@
+package au.com.bluemix.training.appb;
+
+import javax.ejb.Local;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Use;
+
+@Local
+@WebService(targetNamespace="http://appa/ServiceB")
+@SOAPBinding(style=javax.jws.soap.SOAPBinding.Style.RPC, use=Use.LITERAL, parameterStyle=ParameterStyle.WRAPPED)
+public interface IServiceB {
+	
+	int minus(int left, int right);
+	
+}
---
+++ src/main/java/au/com/bluemix/training/appb/ServiceB.java
@@ -0,0 +1,17 @@
+package au.com.bluemix.training.appb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+@Stateless
+@LocalBean
+@WebService(serviceName = "ServiceBService", portName = "ServiceBPort", endpointInterface = "au.com.bluemix.training.appb.IServiceB", targetNamespace = "http://appa/ServiceA")
+public class ServiceB implements IServiceB {
+
+	@Override
+	public int minus(int left, int right) {
+		return left + right;
+	}
+
+}
